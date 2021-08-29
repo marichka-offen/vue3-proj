67d6452b6c91af03c80b78f01c59bae78415c65a
Marichka Offen <marichka.offen@gmail.com>
2021-08-29 19:03:57 -0400

    files for deploy

---
+++ src/views/EventList.vue
@@ -0,0 +1,42 @@
+<template>
+  <h1>Events For Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService.js'
+
+export default {
+  name: 'EventList',
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      events: null,
+    }
+  },
+  created() {
+    // before data mounted; we have access to data, but it's not rendered yet
+    EventService.getEvents()
+      .then((res) => {
+        this.events = res.data
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  },
+}
+</script>
+
+<style>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
